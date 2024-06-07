a28fb9c13a37f79498fa6e4339fc765dd9059e3e
jahardesty <81397442+jahardesty@users.noreply.github.com>
2024-06-07 16:10:51 -0400

    version 1.0

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
   constructor(coords, distance, duration) {
     this.coords = coords; // lat, lng
     this.distance = distance; // in miles
@@ -27,6 +28,9 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+  click() {
+    this.clicks++;
   }
 }
 
@@ -76,11 +80,17 @@ class App {
   #map;
   #mapEvent;
   #workouts = [];
+  #mapZoomLevel = 16;
 
   constructor() {
+    //  Get user's position
     this._getPosition();
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
+    // Get local storage
+    this._getLocalStorage();
   }
 
   _getPosition() {
@@ -96,10 +106,9 @@ class App {
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(latitude, longitude);
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 15);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -107,6 +116,10 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {        
+        this._renderWorkoutMarker(work);
+    })
   }
 
   _showForm(mapE) {
@@ -169,7 +182,6 @@ class App {
     }
     // Add new object to workout array
     this.#workouts.push(workout);
-    console.log(workout);
 
     // Render workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -179,7 +191,12 @@ class App {
 
     // Hide form + clear input fields in form
     this._hideForm();
+
+    // Set local storage to all workouts
+    this._setLocalStorage();
   }
+
+
 
   _renderWorkoutMarker(workout) {
     L.marker(workout.coords)
@@ -222,7 +239,7 @@ class App {
         <div class="workout__details">
           <span class="workout__icon">⚡️</span>
           <span class="workout__value">${workout.pace.toFixed(1)}</span>
-          <span class="workout__unit">min/mi</span>
+          <span class="workout__unit">mi/min</span>
           </div>
            <div class="workout__details">
            <span class="workout__icon">🦶🏼</span>
@@ -236,7 +253,7 @@ class App {
           <div class="workout__details">
            <span class="workout__icon">⚡️</span>
            <span class="workout__value">${workout.speed.toFixed(1)}</span>
-           <span class="workout__unit">km/h</span>
+           <span class="workout__unit">mi/h</span>
          </div>
          <div class="workout__details">
           <span class="workout__icon">⛰️</span>
@@ -246,6 +263,38 @@ class App {
         </li>`;
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if(!workoutEl) return;
+    const workout = this.#workouts.find(work => work.id === workoutEl.dataset.id);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+        animate: true,
+        pan: {
+            duration: 1
+        }
+    });
+    // using public interface
+    // workout.click();
+  }
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'))
+
+    if(!data) return;
+    this.#workouts = data;
+    this.#workouts.forEach(work => {
+        this._renderWorkout(work);
+    })
+  }
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
